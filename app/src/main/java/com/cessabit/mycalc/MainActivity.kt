ad15d299fbe1dea54a86d7763d614188a9317bfb
Serene <serene719@gmail.com>
2023-11-20 19:50:41 +0530

    adding UI changes and clear functionality as well as decimal

--- app/src/main/java/com/cessabit/mycalc/MainActivity.kt
+++ app/src/main/java/com/cessabit/mycalc/MainActivity.kt
@@ -3,16 +3,38 @@ package com.cessabit.mycalc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+    var lastNumber:Boolean = false
+    var lastDot: Boolean =false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        tvInput = findViewById(R.id.tvInput)
     }
     fun onDigit(view:View)
     {
-        Toast.makeText(this,"button Clicked",Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+        lastNumber = true
+        lastDot = false
+    }
+    fun onClear(view:View)
+    {
+        tvInput?.text = ""
+    }
+    fun onDecimalPoint(view: View)
+    {
+        if(lastNumber && !lastDot)
+        {
+            tvInput?.append(".")
+            lastNumber = false
+            lastDot = true
+        }
+
     }
 }
